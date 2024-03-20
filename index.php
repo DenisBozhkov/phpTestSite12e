3dc10b34a1e27e5a74c4de702bebc4f2f5420923
Денис Божков <stu2201261006@uni-plovdiv.bg>
2024-03-20 18:19:55 +0200

    Created index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	function prepair_db($link)
+	{
+		mysqli_query($link,"create database testPHPSite");
+		mysqli_select_db($link,"testPHPSite");
+		$sql="create table users(
+				id int auto_increment primary key,
+				firstname varchar(50) not null,
+				lastname varchar(50) not null)";
+		mysqli_query($link,$sql);
+	}
+	
+	$link=mysqli_connect("localhost","root","");
+	try
+	{
+		mysqli_select_db($link,"testPHPSite");
+	}
+	catch(Exception)
+	{
+		prepair_db($link);
+	}
+?>
